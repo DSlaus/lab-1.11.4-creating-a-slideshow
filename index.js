d1234982727bb36d618f58a5fcb13b06017a36d4
MarsVolta4366 <derek.slauson5@gmail.com>
2021-08-26 20:07:22 -0500

    Finished slideshow activity

--- index.js
+++ index.js
@@ -6,3 +6,65 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+let imagesIndex = 0
+let bubbles = document.querySelectorAll(".bubble")
+let image = document.querySelector("img")
+
+//Sets up page on load
+changeImage()
+fillInBubble()
+
+//Next arrow
+let nextArrow = document.querySelector(".next")
+nextArrow.addEventListener("click", function() {
+    nextSlide()
+})
+
+//Previous arrow
+let previousArrow = document.querySelector(".previous")
+previousArrow.addEventListener("click", function() {
+    previousSlide()
+})
+
+//On bubble click
+bubbles.forEach(addBubbleEventListener)
+
+function nextSlide() {
+    imagesIndex++
+    if(imagesIndex === images.length) {
+        imagesIndex = 0
+    }
+    changeImage()
+    fillInBubble()
+}
+
+function previousSlide() {
+    imagesIndex--
+    if(imagesIndex === -1) {
+        imagesIndex = (images.length - 1)
+    }
+    changeImage()
+    fillInBubble()
+}
+
+function fillInBubble() {
+    for(let i = 0; i < bubbles.length; i++) {
+        bubbles[i].textContent = "○"
+    }
+    let fillBubble = document.querySelector("#button-" + (imagesIndex))
+    fillBubble.textContent = "●"
+}
+
+function addBubbleEventListener(item, index) {
+    bubbles[index].addEventListener("click", function() {
+        imagesIndex = index 
+        changeImage()
+        fillInBubble()
+    })
+}
+
+function changeImage() {
+    image.setAttribute("src", images[imagesIndex])
+}
+
+
